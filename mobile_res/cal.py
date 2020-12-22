d9daaac94fbfc11ab4fada13159d419e586f9ac1
ishanjindal2607 <ijindal@ec.iitr.ac.in>
2020-12-22 22:01:51 +0530

    added calc file

---
+++ mobile_res/cal.py
@@ -0,0 +1,9 @@
+import sys
+import csv
+import os
+print(sys.argv[1])
+with open(sys.argv[1], mode = 'r') as file:
+	reader = csv.DictReader(file)
+	print("conv layer    dram_ifmap   dram_ofmap   dram_filter    sram_read    sram_write")
+	for row in reader:
+		print("{}{}{}{}{}{}\n".format(row['Layer'],row["\tDRAM_IFMAP_bytes"],row["\tDRAM_OFMAP_bytes"],row["\tDRAM_Filter_bytes"],row["\tSRAM_read_bytes"],row["\tSRAM_write_bytes"]))
